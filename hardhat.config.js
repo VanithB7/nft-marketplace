4c048178ea7b16dfced882f97342d53e385dabe8
VanithB7 <97032969+VanithB7@users.noreply.github.com>
2022-04-14 14:39:10 +0530

    creating infura rpc account in influra website.. setting projectid in hardhatconfig file

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,14 +1,17 @@
 require("@nomiclabs/hardhat-waffle");
 
+const fs =require('fs');
+const metamaskPrivateKey=fs.readFileSync('.secret').toString();
+const projectId='2db3642800c6438d8aca8c49c5e6e111';
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+// task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+//   const accounts = await hre.ethers.getSigners();
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
+//   for (const account of accounts) {
+//     console.log(account.address);
+//   }
+// });
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -18,4 +21,18 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+   hardhat:{
+     chainId:1337
+   },
+    mumbai:{
+      url:`https://polygon-mumbai.infura.io/v3/${projectId}`,
+      accounts:[metamaskPrivateKey]
+    },
+    mainnet:{
+      url:`https://polygon-mainnet.infura.io/v3/${projectId}`,
+      accounts:[metamaskPrivateKey]
+    },
+  },
+
 };
